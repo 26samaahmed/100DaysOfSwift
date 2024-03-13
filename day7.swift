deb4d8e48eb95bbf47453b4f34c266c7ba1a8b3c
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-03-12 23:54:14 -0700

    Finished day 7 :)

--- day7.swift
+++ day7.swift
@@ -45,3 +45,69 @@ print(result2)
 
 let result3 = isEqual(val1: "abc", val2: "cba")
 print(result3)
+
+
+// i can also return multiple things in the form of an array
+func name() -> [String] {
+  ["Sama", "Ahmed"]
+}
+let n = name()
+print("My name is \(n[0])")
+
+// Tuples [fixed size array]
+
+func getUser()->(firstName: String, lastName: String) {
+  (firstName: "Sama", lastName: "Ahmed") // i also don't need to explicity specify first name, last name
+  // ("Sama", "Ahmed") will work too
+}
+let user = getUser()
+print("This user's name is \(user.firstName) \(user.lastName)")
+
+// What makes tuples different than dictionaries, is that its guaranteed that there will be values for tuple and I won't need
+// to provide a default one because of the potential of it not existing
+
+// Note: if there was no name given to each value like firstName/lastName, we can still access using indices
+func getUser2() -> (String, String) {
+  ("sama", "salma")
+}
+
+let user2 = getUser2()
+print("This user's name is \(user2.0) or \(user2.1)")
+
+// i can also do this
+let (a, b) = getUser() // if u dont need one of the values, replace it with _
+print("this is \(a)")
+
+// let's say i want to call a function but dont want to expliciitly define the var name when calling the function, i can add
+// underscore before the var name
+
+// Difference like this:
+func getNum(string: String) -> String {
+  return string.uppercased()
+}
+
+// vs.
+
+func getNum2(_ string: String) -> String {
+  return string.uppercased()
+}
+
+var c = getNum(string: "sama")
+var d = getNum2("sama") // for this i didnt need to specify var name before
+print("\(c) vs \(d)")
+
+// when creating functions, for readability i can have an internal name and external name
+func printTimesTable(for num: Int) {
+  for i in 1...12{
+    print("\(i) x \(num) is \(i*num)")
+    
+  }
+}
+
+printTimesTable(for: 5)   // for is the external name and number is the internal name
+
+/*
+Note 2 things:
+1. either specify _ before parameter name if I dont plan on using the external parameter name
+2. Add a second name so that I have an internal and external name
+*/
